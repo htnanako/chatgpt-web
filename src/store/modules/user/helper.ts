4bd45b5ecb6e856243b6152ae18dcdbb3753fdae
htnanako <794046218@qq.com>
2023-06-02 18:41:52 +0800

    update info by htnanako

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://avatars.githubusercontent.com/u/68056603?v=4',
+      name: 'NaNaKo',
+      description: 'View on <a href="https://github.com/htnanako/chatgpt-web" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
